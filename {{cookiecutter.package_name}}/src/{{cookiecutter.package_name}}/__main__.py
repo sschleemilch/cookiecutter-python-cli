6a85589585f895231b03d69175d3ce9b34b63ef8
Sebastian Schleemilch <sebastian.schleemilch@gmail.com>
2022-08-24 21:55:37 +0200

    Made main shorter

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -1,11 +1,11 @@
 import logging
+
 import rich_click as click
 from rich.logging import RichHandler
 from rich.traceback import install
 
 from {{cookiecutter.package_name}} import __version__
 
-install(show_locals=True)
 
 FORMAT = "%(message)s"
 logging.basicConfig(
@@ -15,7 +15,12 @@ logging.basicConfig(
     handlers=[RichHandler(rich_tracebacks=True)]
 )
 
-log = logging.getLogger("{{cookiecutter.package_name}}")
+logger = logging.getLogger("{{cookiecutter.package_name}}")
+
+def configure_run_mode(debug: bool):
+    if debug:
+        logger.setLevel("DEBUG")
+    install(show_locals=debug)
 
 @click.command()
 @click.option("--debug", is_flag=True, help="Enables debug outputs.")
@@ -23,15 +28,16 @@ log = logging.getLogger("{{cookiecutter.package_name}}")
 def main(
     version: bool = False, 
     debug: bool = False):
-    """Console script for {{cookiecutter.package_name}}"""
+    """ main cli entry point """
+
+    configure_run_mode(debug)
+
     if version:
         print(__version__)
         return
-    if debug:
-        log.setLevel("DEBUG")
 
-    log.info(f"Running {{cookiecutter.package_name}}")
-    log.debug(f"Debugging infos")
+    logger.info("Running {{cookiecutter.package_name}}")
+    logger.debug("Debugging infos")
 
 if __name__ == "__main__":
     main()
