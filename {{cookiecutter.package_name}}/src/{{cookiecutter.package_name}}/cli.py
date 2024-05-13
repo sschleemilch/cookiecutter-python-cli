425d91032e86bb12ab661eda874245e730260271
Sebastian Schleemilch (JC-13) <sebastian.schleemilch@bmw.de>
2024-05-13 14:17:45 +0200

    Added log level granularity and log file

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/cli.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/cli.py
@@ -3,6 +3,7 @@ import logging
 import rich_click as click
 from rich.logging import RichHandler
 from rich.traceback import install
+from pathlib import Path
 
 from . import __version__
 
@@ -13,17 +14,22 @@ log = logging.getLogger("{{cookiecutter.package_name}}")
 
 
 @click.command()
-@click.option("-v", "--verbose", is_flag=True, help="Enables verbose output.")
+@click.option("-l", "--log-level", type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], case_sensitive=False), default="INFO", help="Log level", show_default=True)
+@click.option("--log-file", type=click.Path(dir_okay=False, writable=True, path_type=Path), help="Log file")
 @click.version_option(__version__)
-def cli(verbose: bool) -> None:
+def cli(log_level: str, log_file: Path | None) -> None:
+    if log_file:
+        file_handler = logging.FileHandler(log_file, mode="w")
+        file_handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(message)s"))
+        log.addHandler(file_handler)
 
-    if verbose:
-        log.setLevel(logging.DEBUG)
+    log.setLevel(log_level)
+    if log_level == "DEBUG":
         install(show_locals=True)
 
     log.info("Running {{cookiecutter.package_name}}")
     log.debug("Debugging infos")
 
 
-def main():
+def main() -> None:
     cli(auto_envvar_prefix="{{cookiecutter.package_name}}")
